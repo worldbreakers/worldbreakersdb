16892c50007bd2c8f15423bac8bef14a31be7816
tessaluniki <tessa@luniki.de>
2022-03-16 10:13:34 +0100

    Enhance icon.

--- web/js/index.js
+++ web/js/index.js
@@ -1,15 +1,17 @@
 $(document).on('data.app', function() {
-  for (var i = 0; i < Decklist.cards.length; i++) {
-    var slot = Decklist.cards[i];
-    NRDB.data.cards.update({
-      code : slot.card_code
-    }, {
-      indeck : parseInt(slot.qty, 10)
-    });
-  }
+  if (Decklist) {
+    for (var i = 0; i < Decklist.cards.length; i++) {
+      var slot = Decklist.cards[i];
+      NRDB.data.cards.update({
+        code : slot.card_code
+      }, {
+        indeck : parseInt(slot.qty, 10)
+      });
+    }
     if(Decklist.mwl_code) {
         MWL = NRDB.data.mwl.findById(Decklist.mwl_code);
     }
+  }
   update_deck();
 });
 
