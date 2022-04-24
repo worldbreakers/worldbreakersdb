9838202463b7b390703a59653a53bf9bb44c8238
tessaluniki <tessa@luniki.de>
2022-04-24 13:55:52 +0200

    Sort guild distribution before display.

--- app/Resources/assets/js/components/GuildDistribution.svelte
+++ app/Resources/assets/js/components/GuildDistribution.svelte
@@ -1,37 +1,37 @@
 <script>
-  export let distribution;
+export let distribution;
 
-  $: relatives = () => {
-    let sum = 0;
-    Object.entries(distribution).forEach(([guild, count]) => {
-      sum += count;
-    });
+let relatives = [];
 
-    return sum > 0
-      ? Object.entries(distribution).reduce((guilds, [guild, count]) => {
-          guilds[guild] = count / sum;
-          return guilds;
-        }, {})
-      : {};
-  };
+$: {
+    const sum = Object.entries(distribution).reduce((memo, [guild, count]) => {
+        return memo + count;
+    }, 0);
 
-  $: toPercent = (value) => `${value * 100}%`;
+    let guilds = [];
+    if (sum > 0) {
+        guilds = Object.entries(distribution).map(([guild, count]) => ([guild, count / sum]));
+        guilds.sort(([guildA, relA], [guildB, relB]) => relB - relA);
+    }
+    relatives = guilds;
+}
+
+$: toPercent = (value) => `${value * 100}%`;
+
 </script>
 
 <span class="guild-distribution">
-  {#each Object.entries(relatives()) as [guild, relative] (guild)}
-    <span
-      style="background-color: var(--guild-{guild}) !important;width: {toPercent(
-        relative
-      )}"
-      aria-label="{guild} {toPercent(relative)}"
-      class="guild-distribution-item"
-    />
-  {/each}
+    {#each relatives as [guild, relative] (guild)}
+        <span
+            style="background-color: var(--guild-{guild}) !important; width: {toPercent(relative)}"
+            aria-label="{guild} {toPercent(relative)}"
+            class="guild-distribution-item"
+        />
+    {/each}
 </span>
 
 <style>
-  .guild-distribution {
+.guild-distribution {
     display: flex;
     height: 8px;
     overflow: hidden;
@@ -39,8 +39,8 @@
     background-color: rgba(175, 184, 193, 0.2);
     border-radius: 6px;
     outline: 1px solid transparent;
-  }
-  .guild-distribution-item {
+}
+.guild-distribution-item {
     outline: 2px solid transparent;
-  }
+}
 </style>
